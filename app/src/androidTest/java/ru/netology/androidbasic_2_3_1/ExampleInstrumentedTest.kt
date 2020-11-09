da8e2e5627bdc66f76d9398d217be74f41fd7075
teodorro <edem.teo@gmail.com>
2020-11-09 10:47:34 +0300

    project name

--- app/src/androidTest/java/ru/netology/androidbasic_2_3_1/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/netology/androidbasic_2_3_1/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ru.netology.androidbasic_2_1_1", appContext.packageName)
+        assertEquals("ru.netology.androidbasic_2_3_1", appContext.packageName)
     }
 }
